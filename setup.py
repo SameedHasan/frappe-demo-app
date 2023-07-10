c46916d342189f51101adf3a52d42f6f37691acc
SameedHasan <sameedh41@gmail.com>
2023-07-10 14:22:17 +0500

    sec commit

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ with open("requirements.txt") as f:
 	install_requires = f.read().strip().split("\n")
 
 # get version from __version__ variable in demo_app/__init__.py
+# get version from __version__ variable in demo_app/__init__.py
 from demo_app import __version__ as version
 
 setup(
